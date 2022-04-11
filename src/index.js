f7586c748c165f567f1314c46c57b6c72343c17e
manayaise30 <manaya1103@icloud.com>
2022-04-11 12:49:28 +0900

    Comment

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-let yo;
-app.post("/", (req, res) => {
-  yo = req.body;
-  console.log(yo);
-});
-
 app.post("/", (req, res) => {
   const data = req.body;
   console.log("req.body", data);
@@ -19,10 +13,6 @@ app.post("/", (req, res) => {
   app.use(express.urlencoded({ extended: true }));
 });
 
-//なくてよい
-
-
-//app.listen(3000, () => console.log("Listening on port 3000"));
 process.env.NOW_REGION
   ? (module.exports = app)
   : app.listen(PORT, () => console.log(`now listening on port${PORT}`));
