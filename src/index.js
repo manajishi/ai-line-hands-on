30dee2a49555c6dbf1ed4836c6ec6d8ec3e357ab
manayaise30 <manaya1103@icloud.com>
2022-04-11 12:17:07 +0900

    Comment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser=require("body-parser");
 const { application } = require('express');
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
 
-let yo;
-app.post("/api/get",(req,res)=>{
-    yo=req.body;
-    console.log(yo);
-});
-
-app.post("/api/post",(req,res)=>{
-    const data=req.body;
-    const name=data.Name;
-    console.log("name",name);
-    res.send("api/post: Hello World!");
-});
-
-
-app.get('/', function (req, res) {
- res.send('Hello World!');
+app.post('/', function (req, res) {
+ const data=req.body;
+ console.log(req.body,data);
+ res.send('api ok');
 });
 
 
