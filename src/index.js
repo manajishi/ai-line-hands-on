07338e894f7bb3158b8fa578f2b4635e1dfb11a3
manayaise30 <manaya1103@icloud.com>
2022-04-12 21:07:50 +0900

    Content

--- src/index.js
+++ src/index.js
@@ -25,20 +25,21 @@ app.post("/", (req, res) => {
        'Authorization': 'Bearer ' + 'tX5KNspepXrnrZChWBzw1r6HEPyTE1X+iyhvJOjtjLqpm6wLypGnwSSdEFEC3nc5Anj+SwkDtQNJrYOr7rbULBC7mUN72tPwAAzw+w71P1kJbnOcA3ogFpE+spZT66UxaHCSK+84vA+Vfl0aYL3obgdB04t89/1O/w1cDnyilFU=',
     },
     encoding: null
-};
-
-request(options,function(error, response, body) {
-  const buffer = new Buffer.from(body);
-  console.log(buffer);
-  const option={
-    url: "https://leadhack51stcentralindia-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/e5b423cb-c18b-40e4-b640-29eb206e4d86/classify/iterations/Iteration1/image",
-    method: "post",
-    headers:{
-      "Content-Type":"application/octet-stream",
-      "Prediction-Key":"7254f5b6e9004fa6aeb869d0a76dc317",
-    },
-    body: buffer,
   };
+
+  request(options,function(error, response, body) {
+    const buffer = new Buffer.from(body);
+    console.log(buffer);
+    const option={
+      url: "https://leadhack51stcentralindia-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/e5b423cb-c18b-40e4-b640-29eb206e4d86/classify/iterations/Iteration1/image",
+      method: "post",
+      headers:{
+        "Content-Type":"application/octet-stream",
+        "Prediction-Key":"7254f5b6e9004fa6aeb869d0a76dc317",
+      },
+      body: buffer,
+  };
+  console.log(body);
   
 });
 
